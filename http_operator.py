b6e21819b299108483f782ceb0fc93df70eabf28
zackpan <zackpan@hotmail.com>
2020-08-13 23:20:55 -0500

    fix compiler error

--- http_operator.py
+++ http_operator.py
@@ -1,8 +1,20 @@
 from airflow.models import DAG
+from datetime import datetime, timedelta
 from airflow.operators.http_operator import SimpleHttpOperator
 from airflow.utils.dates import days_ago
 
-dag = DAG(dag_id='fis_schedules', schedule_interval=None, tags=['fis'])
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.utcnow(),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(dag_id='fis_schedules', default_args=default_args, schedule_interval=None, tags=['fis'])
 task_get_op = SimpleHttpOperator(
     task_id='get_op',
     method='GET',
